1492c6e7c7c48c68f550c05ead987bfdc861a0ca
thohohong <so0426yeon@gmail.com>
2022-10-19 20:58:22 +0900

    solve 01325

--- q01325_efficientHacking.py
+++ q01325_efficientHacking.py
@@ -1,46 +1,47 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
-def find(a, visit) :
-    cycleFlag = False
-    if check[a] :
-        return computers[a]
-    visit[a] = True
-    for i in computers[a] :
-        if visit[i] :
-            cycleFlag = True
-            continue
-        computers[a] = computers[a].union(find(i, visit))
-    if not cycleFlag :
-        check[a] = True
-    return computers[a]
+def find(a):
+  answer = 0
+  visit = [False for i in range(N+1)]
+  queue = deque()
+
+  queue.append(a)
+  visit[a] = True
+  while(not len(queue) == 0):
+    # print(visit)
+    item = queue.popleft()
+    answer += 1
+    
+    for i in computers[item]:
+      if not visit[i] :
+        queue.append(i)
+        visit[i] = True
+  
+  return answer
+    
 
 input_ = input("").split()
 
 N = int(input_[0])
 M = int(input_[1])
 
-computers = list(set() for i in range(N + 1))
-check = list(False for i in range(N + 1))
+#check = [False for i in range(N+1)]
 
-for i in range(N + 1):
-    computers[i].add(i)
+computers = [[] for i in range(N+1)]
 
 for i in range(M):
-    input_ = input("").split()
-    A = int(input_[0])
-    B = int(input_[1])
-    computers[B].add(A)
+  input_ = input("").split()
+  A = int(input_[0])
+  B = int(input_[1])
+  computers[B].append(A)
 
-maximum = 0
-numList = list(0 for i in range(N + 1))
+hacknum = [0 for i in range(N+1)]
 
-for i in range(N + 1) :
-    visit = [False for i in range(N+1)]
-    cur = find(i, visit).__len__()
-    numList[i] = cur
-    maximum = max(cur, maximum)
+for i in range(1, N+1) :
+  hacknum[i] = find(i)
 
-for i in range(N + 1) :
-    if (numList[i] == maximum) :
-        print(i, end=" ")
+maxNum = max(hacknum)
+
+for i in range(1, N+1):
+  if hacknum[i] == maxNum :
+    print(i, end=" ")
